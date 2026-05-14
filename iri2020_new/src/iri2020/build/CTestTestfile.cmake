c6222564b730630bd47844a432fb8b6a16c4e740
Chunming Wang <cwang@usc.edu>
2026-05-14 16:36:36 -0700

    Update .gitignore

--- iri2020_new/src/iri2020/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/austinhunter/IonosphereTomography/iri2020_new/src/iri2020
-# Build directory: /home/austinhunter/IonosphereTomography/iri2020_new/src/iri2020/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- iri2020_new/src/iri2020/build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/austinhunter/IonosphereTomography/iri2020_new/src/iri2020/src
-# Build directory: /home/austinhunter/IonosphereTomography/iri2020_new/src/iri2020/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
