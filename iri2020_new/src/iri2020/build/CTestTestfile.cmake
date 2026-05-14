3accb1b717d4f4608acef3fa1bffe9478b14fb77
Chunming Wang <cwang@usc.edu>
2026-05-14 16:51:08 -0700

    Update .gitignore

--- iri2020_new/src/iri2020/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/cwang/Documents/Consulting/PlanetIQ/Code/IonosphereTomography/iri2020_new/src/iri2020
-# Build directory: /Users/cwang/Documents/Consulting/PlanetIQ/Code/IonosphereTomography/iri2020_new/src/iri2020/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
